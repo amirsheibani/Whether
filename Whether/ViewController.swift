71232574359e2efca4a4c251761f6622d5ed77f5
AmirSheibani <sheibani.amir@gmail.com>
2018-06-12 10:54:31 +0430

    add project

---
+++ Whether/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whether
+//
+//  Created by amir sheibani on 6/12/18.
+//  Copyright © 2018 amir sheibani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
